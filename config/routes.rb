82b882e3c49860b8167b264d08eb2c0e2c7dcdc7
kmarks2013 <kmarks2013@gmail.com>
2020-10-14 08:29:47 -0400

    adds highscores route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :users
+
+  get '/highscores', to: 'users#high_scores'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
