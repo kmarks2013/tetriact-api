87d7951686db2d4573466e1b4bc510843624f3d2
kmarks2013 <kmarks2013@gmail.com>
2020-10-14 08:34:06 -0400

    adds topscore route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :users
 
   get '/highscores', to: 'users#high_scores'
+  get '/topscore', to: 'users#top_score'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
